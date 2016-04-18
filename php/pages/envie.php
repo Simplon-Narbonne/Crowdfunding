1f722ba7e7cd8000ddd70267dbd3ea3a9af0642c
simplon-philippec <colas.p@hotmail.fr>
2016-04-18 19:09:16 +0200

    ajout page envie dentreprendre

---
+++ php/pages/envie.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Projets Narbonnais</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="../../assets/css/bootstrap/css/bootstrap.css"/>
+    <link rel="stylesheet" href="../../assets/css/normalize.css" type="text/css"/>
+    <link rel="stylesheet" href="../../assets/css/style.css" type="text/css"/>
+    <link rel="stylesheet" href="../../assets/css/stylealternatif.css" type="text/css"/>
+  </head>
+
+<!-- HEADER -->
+<?php
+include('/includes/header.php');
+?>
+
+<!-- BODY -->
+<div class="container">
+<div class="row">
+  <div class="col-lg-9 col-centered" >
+    <!-- [DEBUT] L'idée-->
+    <div class="row">
+      <!-- [DEBUT] Logo Rubrique -->
+  <div class="col-lg-2" >
+     <img class="img-responsive img_entreprise" src="../../assets/images/logo/idee.png"/>
+   </div>
+   <!-- [FIN] Logo Rubrique -->
+
+
+<!-- [DEBUT] GRAND NARBONNE -->
+  <div class="col-lg-5" >
+    <div class="row">
+        <!-- [DEBUT] Logo entreprise -->
+      <div class="col-lg-3" >
+        <a href="http://entreprendre.legrandnarbonne.com/717-iness-des-hommes-des-projets.html">
+          <img class="img-responsive" src="../../assets/images/logo/iness.png"/>
+        </a>
+      </div>
+        <!-- [FIN] Logo entreprise -->
+        <!-- [DEBUT] Nom + Description -->
+      <div class="col-lg-9" >
+        <a class="nom_entreprise">IN’ESS Le Grand Narbonne</a>
+        <p class="adresse_entreprise"> 30 av Pompidor - 11100 Narbonne</p>
+      </div>
+        <!-- [FIN] Nom + Description -->
+    </div>
+  </div>
+        <!-- [DEBUT] Contact entreprise -->
+  <div class="col-lg-5" >
+    <p class="tel_entreprise">TEL : 04 11 23 22 00</p>
+    <a class="mail_entreprise" href="mailto:contact@iness-legrandnarbone.com">contact@iness-legrandnarbone.com</a>
+  </div>
+        <!-- [FIN] Contact entreprise -->
+        <!-- [FIN] GRAND NARBONNE-->
+
+
+<!-- [DEBUT] MLJ -->
+  <div class="col-lg-5">
+    <div class="row">
+        <!-- [DEBUT] Logo entreprise -->
+      <div class="col-lg-3" >
+        <a href="http://www.mljnarbonne.fr/">
+          <img class="img-responsive" src="../../assets/images/logo/mission-locale.png"/>
+        </a>
+      </div>
+        <!-- [FIN] Logo entreprise -->
+        <!-- [DEBUT] Nom + Description -->
+      <div class="col-lg-9" >
+        <a href="http://www.mljnarbonne.fr/" class="nom_entreprise">Mission Locale Jeunes du Grand Narbonne</a>
+        <p class="adresse_entreprise"> 30 av Pompidor - 11100 Narbonne</p>
+      </div>
+        <!-- [FIN] Nom + Description -->
+      </div>
+    </div>
+        <!-- [DEBUT] Contact entreprise -->
+    <div class="col-lg-5">
+        <p class="tel_entreprise">Tél : 04 11 23 22 00</p>
+        <a class="mail_entreprise" href="mailto:mlj@iness-legrandnarbonne.com">mlj@iness-legrandnarbonne.com</a>
+    </div>
+      <!-- [FIN] Contact entreprise -->
+        <!-- [FIN] MLJ -->
+
+
+<!-- [DEBUT] POLE EMPLOI -->
+  <div class="col-lg-5">
+    <div class="row">
+        <!-- [DEBUT] Logo entreprise -->
+      <div class="col-lg-3" >
+        <a href="http://www.pole-emploi.fr/">
+          <img class="img-responsive" src="../../assets/images/logo/pole-emploi.png"/>
+        </a>
+      </div>
+        <!-- [FIN] Logo entreprise -->
+        <!-- [DEBUT] Nom + Description -->
+      <div class="col-lg-9" >
+        <a class="nom_entreprise">Pôle Emploi</a>
+        <p class="adresse_entreprise"> 100 rue Antoine Becquerel - 11100 Narbonne</p>
+      </div>
+        <!-- [FIN] Nom + Description -->
+    </div>
+  </div>
+        <!-- [DEBUT] Contact entreprise -->
+  <div class="col-lg-5">
+    <p class="tel_entreprise">TEL : 39 49</p>
+    <a class="mail_entreprise" href="www.pole-emploi.fr">www.pole-emploi.fr</a>
+  </div>
+        <!-- [FIN] Contact entreprise -->
+
+
+
+    <!-- [FIN] L'idée -->
+  </div>
+
+
+  </div>
+</div>
+</div>
+
+
+<!-- FOOTER -->
+<?php
+include('/includes/footer.php');
+?>
