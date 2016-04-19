904435df4c7f38bcfe98288e30db867e5c722d0b
simplon-renaudV <renaud.vivancos@free.fr>
2016-04-19 10:17:05 +0200

    header.php fini

---
+++ php/includes/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Crowdfunding</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="../../assets/css/bootstrap/css/bootstrap.css"/>
+    <link rel="stylesheet" href="../../assets/css/normalize.css" type="text/css"/>
+    <link rel="stylesheet" href="../../assets/css/style.css" type="text/css"/>
+    <link rel="stylesheet" href="../../assets/css/stylealternatif.css" type="text/css"/>
+​
+  <body>
+
+    <header>
+      <img class='img-responsive' src='../../assets/images/Visuel-principal-png.png'/> 
+      <!--<nav class='elementposition'>
+        <a href='#' class='navlink'>LES PROJETS A FINANCER</a>
+        <a href='projet.php' class='navlink'>LE CROWDFUNDING : COMMENT CA MARCHE ?</a>
+        <a href='aPropos.php' class='navlink'>ENVIE D'ENTREPRENDRE</a>
+      </nav>-->
+    <!--<nav>
+         <ul class="nav nav-pills nav-justified" id="menu">
+             <li ><a href="#" id = "accueil">LES PROJET A FINANCER</a></li>
+             <li><a href="#" id = "projet">LE CROWDFUNDING : COMMENT CA MARCHE ? </a></li>
+             <li><a href="#" id = "apropos">ENVIE D'ENTREPRENDRE</a></li>
+         </ul>
+     </nav>-->
+
+   <nav class="navbar navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          </button>
+        </div>
+      <div id="navbar" class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="../index.php">LES PROJETS A FINANCER</a></li>
+        <li><a href="aPropos.php">LE CROWDFUNDING : COMMENT CA MARCHE ?</a></li>
+        <li><a href="projet.php">ENVIE D'ENTREPRENDRE</a></li>
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</nav>
+
+
+    </header>"
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+ <script src="../../assets/css/bootstrap/js/bootstrap.min.js"></script>
