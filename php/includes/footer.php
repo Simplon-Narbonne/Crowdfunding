b4edaec85e32099b3d43b52a2a1fdaf037ce2384
simplon-renaudV <renaud.vivancos@free.fr>
2016-04-20 09:15:18 +0200

    modif footer et header

---
+++ php/includes/footer.php
@@ -0,0 +1,32 @@
+<footer>
+<div class='row'>
+	<div class='col-lg-4'>
+  		<img src='../../assets/images/logo/Visuel-principal-element-logo-grand-narbonne.png' id='logoNarbonne' class='img-responsive'/>
+	</div>
+	<div class='col-lg-4'>
+		<p class='text-center'>Contact - Mentions légales - A propos</p>
+	</div>
+	<div class='col-lg-4'>
+	 	<img src='../../assets/images/logo/Footer-logo-simplon.png' id='logoSimplon' class='img-responsive'/>
+	</div>
+</div>
+
+<div class='row'>
+	<div class='col-lg-4'>
+
+	</div>
+	<div class='col-lg-4'>
+  		<img src='../../assets/images/logo/logo_tousNosProjet.PNG' id='logoBPI' class='img-responsive'/>
+	</div>
+	<div class='col-lg-4'>
+  		<img src='../../assets/images/logo/iness.png' id='logoIness' class='img-responsive'/>
+	</div>
+</div>
+</footer>
+
+<script src="../../assets/script/jquery-2-2-2.js"></script>
+<script src="../../assets/css/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
+
