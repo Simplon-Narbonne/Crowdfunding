2cfa10ee514d4e2f60e03e3eea4d784db27a1381
simplon-renaudV <renaud.vivancos@free.fr>
2016-04-20 10:58:34 +0200

    ajout header.php

---
+++ php/includes/header.php
@@ -0,0 +1,30 @@
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" href="../../assets/css/bootstrap/css/bootstrap.css"/>
+    <link rel="stylesheet" href="../../assets/css/normalize.css" type="text/css"/>
+    <link rel="stylesheet" href="../../assets/css/style.css" type="text/css"/>
+    <link rel="stylesheet" href="../../assets/css/stylealternatif.css" type="text/css"/>
+​  </head>
+  <body>
+    <header>
+      <img class='img-responsive' src='../../assets/images/logo/Visuel-principal-png.png'/>
+   <nav class="navbar navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          </button>
+        </div>
+      <div id="navbar" class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="../index.php">LES PROJETS A FINANCER</a></li>
+        <li><a href="commentCaMarche.php">LE CROWDFUNDING : COMMENT CA MARCHE ?</a></li>
+        <li><a href="envie.php">ENVIE D'ENTREPRENDRE</a></li>
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</nav>
+</header>"
